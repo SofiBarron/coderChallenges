811f7fe5cd76917ea033829e5506cfe1b3437691
SofiBarron <sofbarron@gmail.com>
2021-09-05 11:35:45 -0300

    Challenge One - Variables

---
+++ challengeOne/main.js
@@ -0,0 +1,8 @@
+let nombre = prompt('Ingrese su nombre');
+let apellido = prompt('Ingrese su apellido');
+
+console.log (nombre, apellido);
+
+let nombreCompleto = nombre + ' ' + apellido;
+
+alert('Hola ' + nombreCompleto);
