e01b1e3243f846ff2007dd3fa23a071d6953cb77
SofiBarron <sofbarron@gmail.com>
2021-10-17 23:34:31 -0300

    JQuery Animations

--- 13.challengeThirteen/main.js
+++ 13.challengeThirteen/main.js
@@ -1,6 +1,8 @@
-//Personal Info Form
-$('#contactDataForm').append(
-    `<div class="form-row">
+//Personal Info - Form
+$('#contactDataContainer').append(
+    `<h2>Contact Information</h2>
+    <form id='contactDataForm'>
+    <div class="form-row">
         <div class="form-group col-md-6">
             <label for="inputName4">Name</label>
             <input type="text" class="form-control" id="inputName4" placeholder="Peter">
@@ -35,41 +37,49 @@ $('#contactDataForm').append(
         </div>
     </div>
     <button type="submit" class="btn btn-primary mb-2" id="js-continueButton">Continue</button>
-    `
-);
-
-
-//Payment Methods Form
-$('#paymentMethodsForm').append(
-    `<form style="display: none">
-            <fieldset class="form-group">
-            <div class="row">
-            <legend class="col-form-label col-sm-2 pt-0">Methods Available:</legend>
-            <div class="col-sm-10">
-            <div class="form-check">
-            <input class="form-check-input" type="radio" name="gridRadios" id="gridRadiosMethod1" value="option1" checked>
-            <label class="form-check-label" for="gridRadios1">
-            PayPal
-            </label>
-            </div>
-            <div class="form-check">
-            <input class="form-check-input" type="radio" name="gridRadios" id="gridRadiosMethod2" value="option2">
-            <label class="form-check-label" for="gridRadios2">
-            Credit Card
-            </label>
-            </div>
-            </div>
-            </div>
-            </fieldset>
-            </form>`
+    </form>`
 );
 
 //Personal Info - Animation
-$('#contactDataForm').fadeIn();
+$('#contactDataForm:hidden').first().fadeIn('slow');
+
+//Payment Methods Form
+$('body').append(`
+    <div id="paymentMethodsForm" style="display: none">
+        <h2>Purchase Methods</h2>
+            <form>
+                <fieldset class="form-group">
+                <div class="row">
+                <legend class="col-form-label col-sm-2 pt-0">Methods Available:</legend>
+                <div class="col-sm-10">
+                <div class="form-check">
+                <input class="form-check-input" type="radio" name="gridRadios" id="gridRadiosMethod1" value="option1" checked>
+                <label class="form-check-label" for="gridRadios1">
+                PayPal
+                </label>
+                </div>
+                <div class="form-check">
+                <input class="form-check-input" type="radio" name="gridRadios" id="gridRadiosMethod2" value="option2">
+                <label class="form-check-label" for="gridRadios2">
+                Credit Card
+                </label>
+                </div>
+                </div>
+                </div>
+                </fieldset>
+                <button class="btn btn-primary" type="reset" id="returnForm">Go back</button>
+            </form>
+        </div>`);
 
 //Continue Button - Animation
+
 $('#contactDataForm').submit(function (e) {
-        e.preventDefault();
-    $('#paymentMethodsForm').show();
-    $('#contactDataForm').hide();
-})
+    e.preventDefault();
+    $('#contactDataContainer').slideUp();
+    $('#paymentMethodsForm').slideDown();
+});
+
+$('#returnForm').click(() => 
+{$('#contactDataContainer').toggle();
+$('#paymentMethodsForm').hide();}
+);
